9aeddb33de6583256934d8aa00e046bab23a9ea0
tyholdren <77170645+tyholdren@users.noreply.github.com>
2021-05-19 20:27:57 -0400

    Add files via upload

---
+++ generator/main.js
@@ -0,0 +1,71 @@
+
+
+document.getElementsByIdName('sad').addEventListener('click', function () {
+  let url = feelingObj['sad'][Math.floor[Math.random() * 10]]
+  this.href = url;
+})
+
+document.getElementsByIdName('happy').addEventListener('click', function () {
+  let url = feelingObj['happy'][Math.floor[Math.random() * 10]]
+  this.href = url;
+})
+
+document.getElementsByIdName('IdkYouTellMe').addEventListener('click', function () {
+  let url = feelingObj['IdkYouTellMe'][Math.floor[Math.random() * 10]]
+  this.href = url;
+})
+
+document.getElementsByIdName('motivated').addEventListener('click', function () {
+  let url = feelingObj['motivated'][Math.floor[Math.random() * 10]]
+  this.href = url;
+})
+
+let feelingObj = {
+  'sad': []
+  'happy': [happylinks],
+  'motivated': [sadFeelinglinks],
+  'IdkYouTellMe':
+}
+
+// get a feeling
+//   in random function we get a random number between 1 and Ten
+// this corresponds to array we want based on
+// Math.random() * lengthoffeelingarrs
+
+
+
+idkYouTellMe[
+  random useless webpage
+alert('what do i look like, a chrom extension.....')
+]
+
+happy[
+  //nigerian prince - lottery - randomly selected lottery winner - balloons
+  hot singles in your area want to chat
+website of puppy gifs
+
+]
+
+like a want to go to a concert[enter: your favorite band ==> redirects to youtube / theband ]
+
+motivated {
+  shia lebouf video - just do it  
+  https://www.youtube.com/watch?v=ZXsQAXx_ao0
+}
+
+sad[
+  // aspca commerical
+  https://www.youtube.com/watch?v=IO9d2PpP7tQ, 
+child slavery
+starving children / unicef commercial
+'you just lost the game website'
+take you to any news website
+all the people richer than you
+https://www.forbes.com/real-time-billionaires/#2995e963d788
+  
+
+]
+
+annoyed[
+  rick roll video on youtube
+]
